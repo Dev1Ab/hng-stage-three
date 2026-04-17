422abb9b9c3d935b1cc140f4e5b44d9c71439df3
sadeeq <sadeejert@gmail.com>
2026-04-17 21:22:35 +0100

    fix trailing slash bug

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,6 @@ from .views import PersonPredictionView, PersonPredictionDetailView
 
 urlpatterns = [
     path("profiles", PersonPredictionView.as_view()),
-    path("profiles/<uuid:id>/", PersonPredictionDetailView.as_view(), name="person-detail"),
+    path("profiles/<uuid:id>", PersonPredictionDetailView.as_view(), name="person-detail"),
 ]
 
