8e30964b925b97879f0282b9bd3d2bd04b0a4e8d
sadeeq <sadeejert@gmail.com>
2026-04-23 12:45:18 +0100

    feat(urls): add natural language query endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from django.urls import path
-from .views import PersonPredictionView, PersonPredictionDetailView
+from .views import PersonPredictionView, PersonPredictionDetailView, ProfileSearchView
 
 urlpatterns = [
     path("profiles", PersonPredictionView.as_view()),
     path("profiles/<uuid:id>", PersonPredictionDetailView.as_view(), name="person-detail"),
+    path("profiles/search", ProfileSearchView.as_view(), name="profile-search"),
 ]
 
