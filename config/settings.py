f6ef6ec6a8c67b60607d2f1497097f99e2941bb0
sadeeq <sadeejert@gmail.com>
2026-04-27 23:22:03 +0100

    refactor(settings): setup jwt tokens

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
