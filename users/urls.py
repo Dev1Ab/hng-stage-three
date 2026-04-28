a2205df23793770e86450911ce23f3f60b43d2d9
sadeeq <sadeejert@gmail.com>
2026-04-28 18:36:12 +0100

    feat(users): implement github Oauth endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import GitHubLoginView, GitHubCallbackView, TokenRefreshView, LogoutView
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('github', GitHubLoginView.as_view(), name='github-login'),
+    path('github/callback', GitHubCallbackView.as_view(), name='github-callback'),
+    path('refresh', TokenRefreshView.as_view(), name='token-refresh'),
+    path('logout', LogoutView.as_view(), name='token-logout'),
 ]
