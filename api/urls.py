35f8ab03b29caa6f0fa6a39de1941f7a64272fd7
sadeeq <sadeejert@gmail.com>
2026-04-28 23:40:19 +0100

    feat(api): add export csv endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from django.urls import path
-from .views import PersonPredictionView, PersonPredictionDetailView, ProfileSearchView
+from .views import ExportProfilesView, PersonPredictionView, PersonPredictionDetailView, ProfileSearchView
 
 urlpatterns = [
     path("profiles", PersonPredictionView.as_view()),
     path("profiles/<uuid:id>", PersonPredictionDetailView.as_view(), name="person-detail"),
     path("profiles/search", ProfileSearchView.as_view(), name="profile-search"),
+    path("api/profiles/export", ExportProfilesView.as_view())
 ]
 
