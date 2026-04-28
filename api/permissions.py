b6ee0e047764f78ac84393724573abfd7786e425
sadeeq <sadeejert@gmail.com>
2026-04-28 23:39:46 +0100

    feat(api): enforce role based access

--- api/permissions.py
+++ api/permissions.py
@@ -3,4 +3,4 @@ from rest_framework import permissions
 class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role == 'admin' and request.user.is_active
+        return request.user and request.user.is_authenticated and request.user.role == 'admin' and request.user.is_staff and request.user.is_active
