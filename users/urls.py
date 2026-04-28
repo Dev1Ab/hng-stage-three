30a4c310ed7d15de11e12fc3785f68914171a150
sadeeq <sadeejert@gmail.com>
2026-04-28 23:36:40 +0100

    feat(users): add user promote endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GitHubLoginView, GitHubCallbackView, TokenRefreshView, LogoutView
+from .views import GitHubLoginView, GitHubCallbackView, TokenRefreshView, LogoutView, CreateAdminView
 
 urlpatterns = [
     path('github', GitHubLoginView.as_view(), name='github-login'),
     path('github/callback', GitHubCallbackView.as_view(), name='github-callback'),
     path('refresh', TokenRefreshView.as_view(), name='token-refresh'),
     path('logout', LogoutView.as_view(), name='token-logout'),
+    path('make-admin/<uuid:user_id>', CreateAdminView.as_view(), name='promote-user'),
 ]
