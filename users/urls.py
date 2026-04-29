ab22c2c15ab73faac1f398b5098bcbbb6bdc9867
sadeeq <sadeejert@gmail.com>
2026-04-29 17:15:05 +0100

    feat(users): add exchange and my profile endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import GitHubLoginView, GitHubCallbackView, TokenRefreshView, LogoutView, CreateAdminView
+from .views import GitHubLoginView, GitHubCallbackView, GitHubExchangeView, MeView, TokenRefreshView, LogoutView, CreateAdminView
 
 urlpatterns = [
     path('github', GitHubLoginView.as_view(), name='github-login'),
     path('github/callback', GitHubCallbackView.as_view(), name='github-callback'),
+    path('exchange', GitHubExchangeView.as_view(), name='github-exchange'),
+    path('me', MeView.as_view(), name='me'),
     path('refresh', TokenRefreshView.as_view(), name='token-refresh'),
     path('logout', LogoutView.as_view(), name='token-logout'),
     path('make-admin/<uuid:user_id>', CreateAdminView.as_view(), name='promote-user'),
