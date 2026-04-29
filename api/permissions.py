e75bdbf8a1acd1bf4f86e74533fbd6906d0705de
sadeeq <sadeejert@gmail.com>
2026-04-29 17:21:54 +0100

    feat(api): enforce active user checks

--- api/permissions.py
+++ api/permissions.py
@@ -4,3 +4,7 @@ class IsAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated and request.user.role == 'admin' and request.user.is_staff and request.user.is_active
+    
+class IsActiveUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_active
