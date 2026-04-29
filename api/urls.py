64e3df14f6945688d7d2da7ee63e20c584b6cb00
sadeeq <sadeejert@gmail.com>
2026-04-29 17:23:31 +0100

    fix(api): handle export

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,16 @@
-from django.urls import include, path
 from django.urls import path
 from .views import ExportProfilesView, PersonPredictionView, PersonPredictionDetailView, ProfileSearchView
 
 urlpatterns = [
-    path("profiles", PersonPredictionView.as_view()),
-    path("profiles/<uuid:id>", PersonPredictionDetailView.as_view(), name="person-detail"),
-    path("profiles/search", ProfileSearchView.as_view(), name="profile-search"),
-    path("api/profiles/export", ExportProfilesView.as_view())
-]
+    path("profiles/export", ExportProfilesView.as_view(), name="profiles-export"),
+    path("profiles/export/", ExportProfilesView.as_view(), name="profiles-export-slash"),
 
+    path("profiles/search", ProfileSearchView.as_view(), name="profile-search"),
+    path("profiles/search/", ProfileSearchView.as_view(), name="profile-search-slash"),
+
+    path("profiles/<uuid:id>", PersonPredictionDetailView.as_view(), name="person-detail"),
+    path("profiles/<uuid:id>/", PersonPredictionDetailView.as_view(), name="person-detail-slash"),
+
+    path("profiles", PersonPredictionView.as_view(), name="profiles"),
+    path("profiles/", PersonPredictionView.as_view(), name="profiles-slash"),
+]
